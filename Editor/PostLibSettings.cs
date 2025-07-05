85e847327ea5983a150679fdc4a58e212429f692
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-07-05 11:00:12 -0300

    fix: update scriptable update settings

--- Editor/PostLibSettings.cs
+++ Editor/PostLibSettings.cs
@@ -2,9 +2,21 @@
 
 namespace PostLib.Editor
 {
-    [CreateAssetMenu(fileName = "PostLibBootstrapSettings", menuName = "PostLib/Bootstrap Settings")]
-    public class PostLibSettings: ScriptableObject
+    [CreateAssetMenu(fileName = "PostLibSettings", menuName = "PostLib/Settings")]
+    public class PostLibSettings : ScriptableObject
     {
-        public bool enableBootstrap = true;
+        [Tooltip("Ativa ou desativa o PostLib nas builds")]
+        public bool enablePostLib = true; // <-- Remova o "static" aqui
+
+        private static PostLibSettings _instance;
+        public static PostLibSettings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = Resources.Load<PostLibSettings>("PostLibSettings");
+                return _instance;
+            }
+        }
     }
 }
