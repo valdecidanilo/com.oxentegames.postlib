b36c6246697756b885a6f8ce729ceb725534cadc
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-08-12 20:46:56 -0300

    fix: update hidratation js lib

--- Editor/PostLibPluginCreator.cs
+++ Editor/PostLibPluginCreator.cs
@@ -19,25 +19,34 @@ mergeInto(LibraryManager.library, {
     });
     },
     JS_Send: function (ptr) {
-        const jsonStr = UTF8ToString(ptr);
-        console.log('[Postlib] raw JSON:', jsonStr);
+      var jsonStr = UTF8ToString(ptr);
+      if (typeof console !== 'undefined' && console.log) console.log('[PostLib] raw JSON:', jsonStr);
 
-        let data;
-        try {
-            data = JSON.parse(jsonStr);
-        } catch (ex) {
-            console.error('[PostLib] JSON inválido', ex);
-            return;
-        }
+      var data;
+      try {
+        data = JSON.parse(jsonStr);
+      } catch (ex) {
+        if (typeof console !== 'undefined' && console.error) console.error('[PostLib] JSON inválido', ex);
+        return;
+      }
 
-        let msg;
-        if (data && typeof data === 'object' && !Array.isArray(data)) {
-          msg = { ...data, internal: true };
-        } else {
-          msg = { payload: data, internal: true };
-        }
-    
+      var isArray = (typeof Array.isArray === 'function')
+        ? Array.isArray(data)
+        : Object.prototype.toString.call(data) === '[object Array]';
+
+      var msg;
+      if (data && typeof data === 'object' && !isArray) {
+        data.internal = true;
+        msg = data;
+      } else {
+        msg = { payload: data, internal: true };
+      }
+
+      try {
         window.parent.postMessage(msg, '*');
+      } catch (err) {
+        if (typeof console !== 'undefined' && console.error) console.error('[PostLib] postMessage falhou:', err);
+      }
     }
 });
 ";
