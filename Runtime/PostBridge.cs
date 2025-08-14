5a4b8eaac4eb2c3ae37ef0eea154d7d13013f5ac
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-08-14 16:42:00 -0300

    fix: update position

--- Runtime/PostBridge.cs
+++ Runtime/PostBridge.cs
@@ -13,17 +13,22 @@ namespace PostLib
 #if !UNITY_EDITOR && UNITY_WEBGL
             JS_Receive();
 #endif
-            Debug.Log("[PostLib] Escutando mensagens.");
+            Debug.Log("[PostLib] Ativando window Listener JS.");
             MessageRouter.SendInitialization();
         }
 
-        public void OnReceive(string json) => MessageRouter.Route(json);
+        public void OnReceive(string json)
+        {
+            Debug.Log("[PostLib] Escutando mensagens.");
+            MessageRouter.Route(json);
+        }
+
         public static void Send(string json)
         {
 #if !UNITY_EDITOR && UNITY_WEBGL
             JS_Send(json);
 #endif
-            Debug.Log($"[PostLib] {json}");
+            Debug.Log($"[PostLib] enviando: {json}");
         }
     }
 }
