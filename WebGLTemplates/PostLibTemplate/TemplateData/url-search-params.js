34ff243b4ca48f368e647705458ad34585026ecc
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-08-25 13:28:40 -0300

    fix: update postlib logo preload

--- WebGLTemplates/PostLibTemplate/TemplateData/url-search-params.js
+++ WebGLTemplates/PostLibTemplate/TemplateData/url-search-params.js
@@ -4,20 +4,30 @@
 }
 
 const regulation = (getURLParameter("regulation") || "asian").toLowerCase();
-
-if (regulation === "brazilian") {
-    window.logoImg.src = "TemplateData/logo_business.png";
-    if (window.productionText) {
-        window.productionText.style.display = "none";
-    }
-}else{
-    window.logoImg.src = "TemplateData/logo_business_2.png";
-}
-
 window.regulation = regulation;
 
-window.addEventListener("load", function () {
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("ServiceWorker.js");
+const chosenLogo =
+regulation === "brazilian"
+    ? "TemplateData/logo_business.png"
+    : "TemplateData/logo_business_2.png";
+
+const logoEl = document.getElementById("unity-logo");
+const prodEl = document.getElementById("production-name");
+
+const preloader = new Image();
+preloader.onload = () => {
+    logoEl.src = chosenLogo;
+    logoEl.style.visibility = "visible";
+
+    if (prodEl) {
+        prodEl.style.display = regulation === "brazilian" ? "none" : "";
+        prodEl.style.visibility = regulation === "brazilian" ? "" : "visible";
     }
+};
+preloader.src = chosenLogo;
+
+window.addEventListener("load", function () {
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("ServiceWorker.js");
+}
 });
