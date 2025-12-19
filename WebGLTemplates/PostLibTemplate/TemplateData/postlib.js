2ea2c381da3401a5bc3da6462e029afa8df2c120
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-12-19 14:42:42 -0300

    fix: comment

--- WebGLTemplates/PostLibTemplate/TemplateData/postlib.js
+++ WebGLTemplates/PostLibTemplate/TemplateData/postlib.js
@@ -1,6 +1,6 @@
 window.addEventListener("message", (event) => {
     const data = event.data;
     if (!data || typeof data !== "object" || !data._type) return;
-    console.log("[PostLib JS] Recebido Unity JS:", response);
-    event.source.postMessage(response, "*");
+    //console.log("[PostLib JS] Recebido Unity JS:", response);
+    //event.source.postMessage(response, "*");
 });
